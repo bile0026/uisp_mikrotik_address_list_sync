c387bc1b8a3b3aa35b4e35d3db4ba94fa0c6c971
bile0026 <zacharybiles@gmail.com>
2023-07-15 22:12:01 -0500

    fix: 🐛 adjust settings import

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 """Module declaration for uisp_mikrotik_address_list_sync."""
 from configparser import ConfigParser
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class UISPMikroTikSyncConfig:
@@ -12,14 +15,17 @@ class UISPMikroTikSyncConfig:
         uisp_config = parser["UISP"]
         mikrotik_config = parser["MIKROTIK"]
 
-        uisp_api_token = uisp_config["token"]
-        uisp_fqdn = uisp_config["server_fqdn"]
+        uisp_api_token = uisp_config.get("token")
+        uisp_fqdn = uisp_config.get("server_fqdn")
         ssl_verify = mikrotik_config["ssl_verify"]
         disable_ssl_warning = mikrotik_config["disable_ssl_warning"]
-        mt_ip = mikrotik_config["router_ip"]
-        mt_username = mikrotik_config["username"]
-        mt_password = mikrotik_config["password"]
+        mt_ip = mikrotik_config.get("router_ip")
+        mt_username = mikrotik_config.get("username")
+        mt_password = mikrotik_config.get("password")
     except Exception as err:
+        logger.error(
+            f"Error loading config from uisp.ini, ensure this file exists and has the proper variables. {err}"
+        )
         raise Exception(
             f"Error loading config from uisp.ini, ensure this file exists and has the proper variables. {err}"
         )
